1beb1c43d31f332e16e97dcf90e31d4ed9b70736
Radhakrishnan <radhakrishnan.asokan@wavemaker.com>
2021-03-15 02:27:42 +0530

    Added phone number

--- src/android/com/callrecord/app/Storage.java
+++ src/android/com/callrecord/app/Storage.java
@@ -3,25 +3,19 @@ package com.callrecord.app;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+
+import com.callrecord.services.RecordingService;
 
 public class Storage {
     private static final String TAG = Storage.class.getSimpleName();
 
     protected static boolean permittedForce = false; // bugged phones has no PackageManager.ACTION_REQUEST_PERMISSIONS activity. allow it all.
+    public static final String SEPERATOR = "_";
 
-    public static final String SCHEME_PACKAGE = "package";
-
-    public static final String COLON = ":";
-
-
-    // String functions
 
     public static boolean permitted(Context context, String[] ss) {
         if (permittedForce)
@@ -55,11 +49,24 @@ public class Storage {
         return true;
     }
 
-    public static void showPermissions(Context context) {
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        Uri uri = Uri.fromParts(SCHEME_PACKAGE, context.getPackageName(), null);
-        intent.setData(uri);
-        context.startActivity(intent);
+
+
+
+    public static String getFormatted( String phone, String contact, String call) {
+        String format = "";
+        switch (call) {
+            case RecordingService.CALL_IN:
+                format += "Incoming" + SEPERATOR;
+                break;
+            case RecordingService.CALL_OUT:
+                format += "Outgoing" + SEPERATOR;
+                break;
+        }
+        if (phone != null && !phone.isEmpty())
+            format += phone + SEPERATOR;
+        if (contact != null && !contact.isEmpty())
+            format += contact + SEPERATOR;
+        format += System.currentTimeMillis();
+            return format;
     }
 }
